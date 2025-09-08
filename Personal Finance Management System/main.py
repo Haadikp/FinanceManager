88cec994e51a10ff9e157d65e11c718414b39221
Hadi k p <hadikpkuniyil@gmail.com>
2025-09-08 20:16:08 +0530

    Updated main.py

--- Personal Finance Management System/main.py
+++ Personal Finance Management System/main.py
@@ -135,8 +135,8 @@ app.config['SECRET_KEY'] = 'qwertyuiop'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'kpgadgetsarena@gmail.com'
-app.config['MAIL_PASSWORD'] = 'voxo isgt wxoi sqeb'
+app.config['MAIL_USERNAME'] = 'your_email@gmail.com'
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_DEBUG'] = True
 mail = Mail(app)
 
@@ -274,7 +274,7 @@ def feedback():
     message_content = request.form.get('message')
 
     # Define the admin's email
-    admin_email = 'kpgadgetsarena@example.com'  # Replace with actual admin email
+    admin_email = 'email@example.com'  # Replace with actual admin email
 
     # Send feedback via email
     msg = Message(f"New Contact Us Message: {subject}",
